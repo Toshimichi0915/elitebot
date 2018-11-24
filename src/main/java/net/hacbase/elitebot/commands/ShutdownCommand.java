80f0428f037ba31783b22e44131619eab06d366e
Toshimichi0915 <toshimichi@hacbase.net>
2018-11-25 07:02:28 +0900

    shutdownコマンド時にメッセージを表示するよう変更

--- src/main/java/net/hacbase/elitebot/commands/ShutdownCommand.java
+++ src/main/java/net/hacbase/elitebot/commands/ShutdownCommand.java
@@ -18,6 +18,7 @@ public class ShutdownCommand extends AdminCommand{
 
     @Override
     public void conduct(EliteUser admin, String[] args) {
+        admin.sendMessage("EliteBotを終了させます");
         bot.getJDA().shutdown();
     }
 }
