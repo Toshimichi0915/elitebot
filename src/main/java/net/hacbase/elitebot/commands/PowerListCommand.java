7650a4ddf97ef7326d1ebe79b6386a055c14bffb
Igaguri <igagurimk@gmail.com>
2019-02-19 14:12:49 +0900

    ?pplist の結果ソートを作成

--- src/main/java/net/hacbase/elitebot/commands/PowerListCommand.java
+++ src/main/java/net/hacbase/elitebot/commands/PowerListCommand.java
@@ -5,6 +5,7 @@ import net.hacbase.elitebot.EliteBot;
 import net.hacbase.elitebot.discord.ElitePower;
 import net.hacbase.elitebot.discord.EliteUser;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class PowerListCommand implements EliteCommand, CommandDescription {
@@ -22,19 +23,23 @@ public class PowerListCommand implements EliteCommand, CommandDescription {
 
     @Override
     public void execute(EliteUser user, String[] args) {
-        StringBuilder builder = new StringBuilder("現在の勢力一覧:\n");
         Collection<ElitePower> powers = bot.getElitePowerProvider().getElitePowers();
         if (powers.size() == 0) {
             user.sendMessage("現在有効な勢力は存在しません");
             return;
         }
 
+        ArrayList<PowerCount> power_list = new ArrayList<>();
         for (ElitePower power : bot.getElitePowerProvider().getElitePowers()) {
-            builder.append(power.getName());
-            builder.append('(');
+            String name = power.getName();
             Role r = bot.getJDA().getRoleById(power.getId());
-            builder.append(r.getGuild().getMembersWithRoles(r).size());
-            builder.append("人)\n");
+            int count = r.getGuild().getMembersWithRoles(r).size();
+            power_list.add(new PowerCount(name, count));
+        }
+
+        StringBuilder builder = new StringBuilder("現在の勢力一覧:\n");
+        for (PowerCount count : power_list) {
+            count.format(builder);
         }
         user.sendMessage(builder.toString());
     }
@@ -42,5 +47,38 @@ public class PowerListCommand implements EliteCommand, CommandDescription {
     @Override
     public String getDescription() {
         return "現在の勢力一覧を表示します";
+    }
+}
+
+class PowerCount implements Comparable<PowerCount> {
+    private String name;
+    private int count;
+
+    PowerCount(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    void format(StringBuilder builder) {
+        builder.append(this.count);
+        builder.append(" :  ");
+        builder.append(this.name);
+    }
+
+    String name() {
+        return this.name;
+    }
+
+    int count() {
+        return this.count;
+    }
+
+    public int compareTo(PowerCount o) {
+        int d = this.count - o.count();
+        if (d != 0) {
+            return d;
+        }
+
+        return this.name.compareTo(o.name());
     }
 }
